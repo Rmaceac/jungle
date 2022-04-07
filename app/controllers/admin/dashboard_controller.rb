5283e485dac4c5f471a9a8fb62a3959148d62f0b
Ryan <ryan.maceachern@gmail.com>
2022-04-07 12:32:51 -0700

    admin dashboard now displays basic product stats and a clickable link

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,6 +3,7 @@ class Admin::DashboardController < ApplicationController
   
   def show
     @products_count = Product.count(:all)
+    @categories_count = Category.count(:all)
   end
 
   protected
