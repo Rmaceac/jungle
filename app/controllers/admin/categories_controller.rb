0845ab5a8d905deba13a08b6799096fdba31f03c
Ryan <ryan.maceachern@gmail.com>
2022-04-07 17:04:00 -0700

    now only routing index, create and new

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,24 @@
 class Admin::CategoriesController < ApplicationController
+  before_filter :authenticate
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+  
+  end
+
+
+  protected
+    def authenticate 
+      authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+      end
+    end
+
 end
